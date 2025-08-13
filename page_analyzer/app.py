842e827757dee53521b660d1f03ae8e82e7d6ec8
sssspoddub <spoddub@yahoo.com>
2025-08-13 13:29:30 +0300

    update app

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, redirect, url_for, flash
 from page_analyzer import database, checker, validator
+from page_analyzer.checker import CheckerError
 
 load_dotenv()
 app = Flask(__name__)
@@ -17,20 +18,24 @@ def index():
 def urls():
     if request.method == 'POST':
         url_input = request.form.get('url')
+
         if not validator.is_valid_url(url_input):
-            error = 'Некорректный URL' if url_input else 'Заполните это поле.'
-            flash(error, 'danger')
-            return render_template('index.html'), 422
+            flash('Некорректный URL' if url_input else 'Заполните это поле', 'danger')
+            return render_template('index.html'), 422                                     # <<< ПОТОМ return
+
         parsed_url = validator.normalize_url(url_input)
         url_id, error = database.add_url(parsed_url)
+
         if error:
-            flash(error, 'warning')
+            flash(error, 'warning')                                
             return redirect(url_for('url_detail', id=url_id))
+
         flash('Страница успешно добавлена', 'success')
         return redirect(url_for('url_detail', id=url_id))
 
     urls = database.get_all_urls()
     return render_template('urls.html', urls=urls)
+
 
 
 @app.route('/urls/<int:id>', methods=['GET', 'POST'])
@@ -39,14 +44,15 @@ def url_detail(id):
     if not url:
         flash('URL не найден', 'danger')
         return redirect(url_for('urls'))
+
     if request.method == 'POST':
         try:
-            status_code, h1, title, description = checker.check_url(
-                url['name']
-            )
+            status_code, h1, title, description = checker.check_url(url['name'])
+        except CheckerError:
+            flash('Произошла ошибка при проверке', 'danger')
+        else:
             database.add_check(id, status_code, h1, title, description)
             flash('Страница успешно проверена', 'success')
-        except checker.CheckError:
-            flash('Произошла ошибка при проверке', 'danger')
         return redirect(url_for('url_detail', id=id))
+
     return render_template('url_detail.html', url=url)
